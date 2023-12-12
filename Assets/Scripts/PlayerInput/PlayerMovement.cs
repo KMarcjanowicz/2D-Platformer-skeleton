4bd00d35fecf2ff3cbba4293a711647f827c8504
KMarcjanowicz <51883166+KMarcjanowicz@users.noreply.github.com>
2023-12-12 21:59:30 +0100

    Simple movement implemented
    Simple movement
    
    - left/right move
    - jump
    
    And a lot of bugs!

--- Assets/Scripts/PlayerInput/PlayerMovement.cs
+++ Assets/Scripts/PlayerInput/PlayerMovement.cs
@@ -1,15 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     // RigidBody2D component - responsible for physics for th eplayer object
-    private Rigidbody2D Rigidbody2DComponent;
+    private Rigidbody2D Rigidbody2DComponent = null;
 
     // responsible to display of play has jumped and is currenlty in air
     private bool IsInAir = false;
+
+    // vector responsible for moving the player - add velocity to the vector direction
+    private Vector2 MoveVector = Vector2.zero;
+
+    private float MoveSpeed = 7.0f;
+
 
 
     private void Start()
@@ -17,17 +25,32 @@ public class PlayerMovement : MonoBehaviour
         Rigidbody2DComponent = GetComponent<Rigidbody2D>();
     }
 
+    private void FixedUpdate()
+    {
+        Rigidbody2DComponent.velocity = new Vector2(MoveVector.x * MoveSpeed, Rigidbody2DComponent.velocity.y);
+    }
+
     public void OnMovement(InputAction.CallbackContext context)
     {
-        if (!context.started) return;
-
-        Debug.Log(context.control);
+        if (context.started)
+        {
+            MoveVector = context.ReadValue<Vector2>();
+        }
+        else if(context.canceled)
+        {
+            MoveVector = Vector2.zero;
+        }
+        else
+        {
+            return;
+        }
+        
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
         if (!context.started) return;
 
-        GetComponent<Rigidbody2D>().velocity = new Vector3(0, 7.0f, 0);
+        Rigidbody2DComponent.velocity = new Vector2(Rigidbody2DComponent.velocity.x, 7.0f);
     }
 }
