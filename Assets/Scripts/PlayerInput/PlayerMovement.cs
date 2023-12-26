ca1857b5a89bc11ab9830a6980474ca6c1e1c6c5
KMarcjanowicz <51883166+KMarcjanowicz@users.noreply.github.com>
2023-12-26 12:53:33 +0100

    player movement finished, entry point to adding animations

--- Assets/Scripts/PlayerInput/PlayerMovement.cs
+++ Assets/Scripts/PlayerInput/PlayerMovement.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
 
 public class PlayerMovement : MonoBehaviour
 {                        
@@ -20,7 +21,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform CeilingCheck = null;
 
     // A collider that will be disabled when crouching
-    [SerializeField] private Collider2D m_CrouchDisableCollider = null;
+    [SerializeField] private Collider2D CrouchDisableCollider = null;
 
     // Amount of maxSpeed applied to crouching movement. 1 = 100%
     [Range(0, 1)][SerializeField] private float CrouchSpeed = .36f;
@@ -37,23 +38,88 @@ public class PlayerMovement : MonoBehaviour
     // vector responsible for moving the player - add velocity to the vector direction
     private Vector2 MoveVector = Vector2.zero;
 
+    // air conctrol
+    [SerializeField] bool AirControl = false;
+
     private bool FacingRight = true;
 
     private bool IsGrounded = true;
+    private bool IsInAir = false;
 
     private bool IsCrouching = false;
+    private bool WantsToCrouch = false;
+
+    private bool IsJumping = false;
+    private bool WantsToJump = false;
 
 
     private void Start()
     {
         Rigidbody2DComponent = GetComponent<Rigidbody2D>();
-
-
     }
 
     private void FixedUpdate()
     {
-        Rigidbody2DComponent.velocity = new Vector2(MoveVector.x * MoveSpeed * Time.fixedDeltaTime, Rigidbody2DComponent.velocity.y);
+        IsGrounded = false;
+        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+        Collider2D[] groundColliders = Physics2D.OverlapCircleAll(GroundCheck.position, .2f, WhatIsGround); ;
+        for (int i = 0; i < groundColliders.Length; i++)
+        {
+            if (groundColliders[i].gameObject != gameObject)
+            {
+                IsGrounded = true;
+            }
+        }
+
+        if(WantsToCrouch)
+        {
+            if(IsGrounded)
+            {
+                IsCrouching = true;
+            }   
+        }
+        else{
+            if(IsGrounded)
+            {
+                Debug.Log(Physics2D.OverlapCircle(CeilingCheck.position, .5f, WhatIsGround));
+
+                // The player must crouch if a circlecast to the ceilingcheck position hits anything designated as ground
+                // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+                // If the character has a ceiling preventing them from standing up, keep them crouching
+                if (Physics2D.OverlapCircle(CeilingCheck.position, .5f, WhatIsGround))
+                {
+                    IsCrouching = true;
+                }
+                else
+                {
+                    IsCrouching = false;
+                }
+            }    
+        }
+
+        if (IsGrounded || AirControl) {
+            if (IsCrouching) //&& is grounded for small jums wwhile crouch but it doesn't work when going from crouch -> stand
+            {
+                // Disable one of the colliders when crouching
+                if (CrouchDisableCollider != null)
+                {
+                    CrouchDisableCollider.enabled = false;
+                }
+                // Move speed while crouching
+                Rigidbody2DComponent.velocity = new Vector2(MoveVector.x * MoveSpeed * CrouchSpeed * Time.fixedDeltaTime, Rigidbody2DComponent.velocity.y);
+            }
+            else
+            {
+                // Enable the collider when not crouching
+                if (CrouchDisableCollider != null)
+                {
+                    CrouchDisableCollider.enabled = true;
+                }
+                //Move speed while normal moving
+                Rigidbody2DComponent.velocity = new Vector2(MoveVector.x * MoveSpeed * Time.fixedDeltaTime, Rigidbody2DComponent.velocity.y);
+            }
+        }
     }
 
     public void OnMovement(InputAction.CallbackContext context)
@@ -90,14 +156,46 @@ public class PlayerMovement : MonoBehaviour
     {
         if (!context.started) return;
 
-        // check if grounded()
-        Rigidbody2DComponent.velocity = new Vector2(Rigidbody2DComponent.velocity.x, JumpForce);
+        // check if grounded
+        if (IsGrounded)
+        {       
+            Rigidbody2DComponent.velocity = new Vector2(Rigidbody2DComponent.velocity.x, JumpForce);
+            IsInAir = true;
+            IsGrounded = false;
+        }
+        
     }
 
     public void OnCrouch(InputAction.CallbackContext context)
     {
-        if (context.started) IsCrouching = true;
-        if(context.canceled) IsCrouching = false;
+        if (context.started)
+        {
+            Debug.Log("Crouch");
+            WantsToCrouch = true;
+        }
+        if (context.canceled) {
+            Debug.Log("Uncrouch");
+            WantsToCrouch = false; 
+        }
+/*
+ *              if (context.started)
+                {
+                    Debug.Log("Crouch");
+                    IsCrouching = true;
+                }
+                if (context.canceled)
+                {
+                    Debug.Log("Uncrouch");
+
+                    // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+                    // If the character has a ceiling preventing them from standing up, keep them crouching
+                    if (!Physics2D.OverlapCircle(CeilingCheck.position, .2f, WhatIsGround))
+                    {
+                        IsCrouching = false;
+                    }
+                }
+        */
+
     }
 
     private void Flip()
